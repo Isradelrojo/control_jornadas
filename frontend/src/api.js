1ef1f60df391a57ff649f3077a0d9d481014f783
Isradelrojo <isradelrojo@gmail.com>
2026-05-24 18:38:07 -0300

    cambiando la URL base de axios

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Creamos una instancia de axios para no repetir la URL en cada archivo
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api' 
+    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
 });
 
 export default api;
