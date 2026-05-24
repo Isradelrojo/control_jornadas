26a1f6ab40ba2eb9409de6d00dde4001facc08e0
Isradelrojo <isradelrojo@gmail.com>
2026-05-24 19:00:13 -0300

    terminando configuraciones

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
+console.log("La URL que está usando Axios es:", import.meta.env.VITE_API_URL);
+
 // Creamos una instancia de axios para no repetir la URL en cada archivo
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
