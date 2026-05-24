df0bca52beeb78dfc675738903e60d4228263edd
Isradelrojo <isradelrojo@gmail.com>
2026-05-24 20:45:20 -0300

    prueba de conexion

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,8 @@ console.log("La URL que está usando Axios es:", import.meta.env.VITE_API_URL);
 
 // Creamos una instancia de axios para no repetir la URL en cada archivo
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
+    //baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000'
+    baseURL: 'https://control-jornadas.onrender.com'
 });
 
 export default api;
