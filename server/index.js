bc66fff1c420b2d0fea014e8d466cf3ed0686d33
Isradelrojo <isradelrojo@gmail.com>
2026-05-24 20:49:17 -0300

    prueba de conexion2

--- server/index.js
+++ server/index.js
@@ -9,7 +9,12 @@ const app = express();
 conectarDB();
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json()); // Para que el servidor entienda JSON
 
 app.use('/api/jornadas', require('./routes/jornadas'));
